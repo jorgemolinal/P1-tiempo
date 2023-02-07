3e8637f2fa06fde4c3ebe049dd910826961906c7
Jorge Molina Lafuente <105129306+jorgemolinal@users.noreply.github.com>
2023-02-07 12:52:15 +0100

    Update config_default.js

--- src/config/config_default.js
+++ src/config/config_default.js
@@ -2,9 +2,9 @@ const CONFIG = {
   server_url: "https://api.openweathermap.org/data/2.5/onecall",
   api_key: "<YOUR_API_KEY>",
   num_items: 4,
+  use_server: true,
   default_lat: 40.416775,
-  default_lon: -3.703790,
-  use_server: true
+  default_lon: -3.703790
 }
 
 export default CONFIG;
